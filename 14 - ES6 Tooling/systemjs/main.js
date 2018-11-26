148e25bc322d4015001e14fce13807376a99ab32
Gel Studio <devondaviau@yahoo.ca>
2018-11-26 09:13:07 -0500

    babel shizz

--- 14 - ES6 Tooling/systemjs/main.js
+++ 14 - ES6 Tooling/systemjs/main.js
@@ -1,3 +1,7 @@
-import { sum, kebabCase } from 'lodash';
+// import { sum, kebabCase } from 'npm:lodash';
 
 // console.log(kebabCase('Devon is the best'));
+
+import { addTax } from './checkout';
+
+console.log(addTax(5, 13));
